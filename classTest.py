bb03e0e8a5c493c9ce63ad8ff1933b488d7a03e7
tvictor20 <tvictor20@germantownfriends.org>
2017-11-10 14:45:47 -0500

    update

--- classTest.py
+++ classTest.py
@@ -1,10 +1,23 @@
 class Student():
+    counter = 0
 
     def __init__(self, name, lastName):
         self.name = name
         self.lastName = lastName
+        self.number = Student.counter
+        Student.counter += 1
+
+    def numberIn(self):
+        self.number += 1
+
+
 
 
 tommy = Student("Tommy", "Victor")
+ben = Student("Ben", "Rosin")
+javier = Student("Javier", "Carmona")
 
-print tommy.name
+javier.numberIn()
+print tommy.number
+print ben.number
+print javier.number
