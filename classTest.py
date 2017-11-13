e2f5e96997b9e3351f70f693c8811ea6684d391c
tvictor20 <tvictor20@germantownfriends.org>
2017-11-13 14:19:34 -0500

    stuff

--- classTest.py
+++ classTest.py
@@ -1,29 +1,37 @@
 class Student():
     counter = 0
-    boyCounter = 0
+    maleCounter = 0
     def __init__(self, name, lastName, gender):
         self.name = name
         self.lastName = lastName
         self.number = Student.counter
-        if gender == "boy":
-            Student.boyCounter += 1
+        if gender == "male":
+            Student.maleCounter += 1
         Student.counter += 1
 
-    def numberIn(self):
-        self.number += 1
+studentList = []
+tommy = Student("Tommy", "Victor", "male")
+studentList.append(tommy)
+ben = Student("Ben", "Rosin", "male")
+studentList.append(ben)
+
+def main():
+    while True:
+        print "There are " + str(tommy.counter) + " students in the directory"
+        print "Press 1 to see everyone in the directory"
+        print "Press 2 to add someone to the directory"
+        x = input()
+        print ""
+        if x == 1:
+            for i in studentList:
+                print i.name + " " + i.lastName
+
+        if x == 2:
+            fname = raw_input("What is their first name? ")
+            lname = raw_input("What is their last name? ")
+            gender = raw_input("What is their gender?")
+            fname = Student(fname, lname, gender)
 
 
 
-
-tommy = Student("Tommy", "Victor", "boy")
-ben = Student("Ben", "Rosin", "none")
-javier = Student("Javier", "Carmona", "boy")
-jesse = Student("Jesse", "McilHenny", "none")
-
-javier.numberIn()
-print tommy.boyCounter
-print javier.boyCounter
-print tommy.number
-print ben.number
-print javier.number
-print jesse.number
+main()
